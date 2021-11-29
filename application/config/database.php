dc6feba3b41d5d787e608feb2bbadd3723cf0304
Munkensm4t <73930504+Munkensm4t@users.noreply.github.com>
2021-11-29 14:23:33 +0700

    Update database.php

--- application/config/database.php
+++ application/config/database.php
@@ -75,7 +75,7 @@ $query_builder = TRUE;
 
 $db['default'] = array(
 	'dsn'	=> '',
-	'hostname' => 'http://lmsjp.eastus.cloudapp.azure.com/',
+	'hostname' => 'localhost',
 	'username' => 'root',
 	'password' => '',
 	'database' => 'lms',
